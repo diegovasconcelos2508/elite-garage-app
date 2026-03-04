2f5847a57a8fb93ac6fa3d1a5c41e7bbd6d5f111
diegovasconcelos2508 <diegovasconcelos2508@gmail.com>
2026-03-04 00:48:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,44 +1,37 @@
-const CACHE_NAME = "elite-garage-v2";
+const CACHE_NAME = "elite-garage-v3";
 
 const urlsToCache = [
-"./",
-"./index.html",
-"./manifest.json",
-"https://cdn.jsdelivr.net/npm/chart.js"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-return response || fetch(event.request)
-.then(fetchResponse => {
-return caches.open(CACHE_NAME).then(cache => {
-cache.put(event.request, fetchResponse.clone());
-return fetchResponse;
-});
-});
-})
-);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
 
 self.addEventListener("activate", event => {
-event.waitUntil(
-caches.keys().then(cacheNames => {
-return Promise.all(
-cacheNames.map(cache => {
-if (cache !== CACHE_NAME) {
-return caches.delete(cache);
-}
-})
-);
-})
-);
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
